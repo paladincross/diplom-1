b2a45a71b8e4a178b35efca6c44c29b638cb4630
Roman_Moravetskyi <Roman_Moravetskui@epam.com>
2014-12-17 17:54:26 +0200

    Clear symfony with created clear bundle

---
+++ app/cache/prod/twig/0e/8f/8f023b6bb5eee77e5f231e5ee7490a2691c325b3f7abafc245740de23ea8.php
@@ -0,0 +1,39 @@
+<?php
+
+/* FSNBotBundle:Default:index.html.twig */
+class __TwigTemplate_0e8f8f023b6bb5eee77e5f231e5ee7490a2691c325b3f7abafc245740de23ea8 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : null), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "FSNBotBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ src/FSN/BotBundle/FSNBotBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FSN\BotBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FSNBotBundle extends Bundle
+{
+}
